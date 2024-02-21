6e6e0d0c39d343c2e0c2eec732a0ab3e25517094
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2024-02-21 20:20:45 +0100

    6

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,100 @@
 #!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
+"""
+Module containing the BaseModel class.
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
 import uuid
+import models
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+
+
+Base = declarative_base()
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
+    """
+    The BaseModel class for the cmd Airbnb console project.
+
+    Attributes:
+        id (str): The unique identifier for each instance.
+        created_at: The datetime representing instance creation time.
+        updated_at: The datetime representing instance last update time.
+    """
+
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
+        """
+        Initializes a new instance of the BaseModel class.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        If kwargs are provided, it sets the attributes of the instance
+        based on the key-value pairs in kwargs.
+        If 'created_at' or 'updated_at' are in kwargs,
+        it converts them to datetime objects.
+        If kwargs are not provided, it generates a unique id
+        and the current datetime for 'created_at' and 'updated_at'.
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        """
+        Returns a string representation of the instance.
+
+        The string includes the class name, id, and dictionary of the instance.
+        """
+        return (
+            f"[{self.__class__.__name__}] "
+            f"({self.id})"
+            f"{self.__dict__}"
+        )
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
-        from models import storage
+        """
+        Updates the 'updated_at' attribute to current datetime
+        and saves the instance.
+        """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        """
+        Returns a dictionary representation of the instance.
+
+        The dictionary includes all attributes of the instance.
+        If 'created_at' or 'updated_at' are attributes of the instance
+        and are datetime objects, it converts them to ISO format strings.
+        """
+
+        dict = self.__dict__
+        dict['__class__'] = self.__class__.__name__
+
+        if 'created_at' in dict and isinstance(dict['created_at'], datetime):
+            dict['created_at'] = dict['created_at'].isoformat()
+        if 'updated_at' in dict and isinstance(dict['updated_at'], datetime):
+            dict['updated_at'] = dict['updated_at'].isoformat()
+
+        return dict
+
+    def delete(self):
+        """ delete object
+        """
+        models.storage.delete(self)
